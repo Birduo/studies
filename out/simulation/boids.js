cb2d11c1556cfdc03dd242dbb20e1ee49abe595c
Dylan Kneidel <dckneidel@ku.edu>
2024-04-26 18:01:05 -0500

    added boids

--- out/simulation/boids.js
+++ out/simulation/boids.js
@@ -5,37 +5,141 @@ let height = window.innerHeight * .40
 
 let boids = []
 
+const size = Math.min(width, height) / 20
+const separation_distance = size * 2
+console.log(`Size: ${size}`)
+console.log(`Separation distance: ${separation_distance}`)
+
+let boid_count = Math.min(100, Math.floor(width / size))
+
 class Boid {
     constructor(x, y) {
-        this.x = x
-        this.y = y
-        this.vx = random(-1, 1)
-        this.vy = random(-1, 1)
-        this.angle = random(360)
+        this.pos = createVector(x, y)
+        this.vel = p5.Vector.random2D()
+        this.accel = createVector()
+        this.maxVel = 2
+        this.angle = 90
+        this.maxForce = 0.03 // max steering force
+    }
 
-        this.close_dx = 0
-        this.close_dy = 0
+    // boids try to match velocity with nearby boids
+    align(others) {
+        let steering = createVector()
+        let total = 0
+        for (let other of others) {
+            let d = dist(this.pos.x, this.pos.y, other.pos.x, other.pos.y)
+            // add to steering if in neighborhood
+            if (other != this && d < this.angle) {
+                steering.add(other.vel)
+                total++
+            }
+        }
+        
+        // align with the average heading if flockmates in neighborhood
+        if (total > 0) {
+            steering.div(total)
+            steering.setMag(this.maxVel)
+            steering.sub(this.vel)
+            steering.limit(this.maxForce)
+        }
+
+        return steering
+    }
+
+    // boids try to move towards the center of mass of nearby boids
+    cohere(others) {
+        let steering = createVector()
+        let total = 0
+        for (let other of others) {
+            // get distance between this boid and the other
+            let d = dist(this.pos.x, this.pos.y, other.pos.x, other.pos.y)
+            if (other != this && d < this.angle) {
+                steering.add(other.pos)
+                total++
+            }
+        }
+
+        // move towards the average position of flockmates in neighborhood
+        if (total > 0) {
+            steering.div(total)
+            steering.sub(this.pos)
+            steering.setMag(this.maxVel)
+            steering.sub(this.vel)
+            steering.limit(this.maxForce)
+        }
+
+        return steering
+    }
+
+    // boids try to keep a small distance from other boids
+    separate(others) {
+        let steering = createVector()
+        let total = 0
+        for (let other of others) {
+            let d = dist(this.pos.x, this.pos.y, other.pos.x, other.pos.y)
+            if (other != this && d < separation_distance) {
+                // track the difference between the two positions
+                let diff = p5.Vector.sub(this.pos, other.pos)
+                diff.div(d)
+                steering.add(diff)
+                total++
+            }
+        }
+
+        // move away from nearby flockmates
+        if (total > 0) {
+            steering.div(total)
+            steering.setMag(this.maxVel)
+            steering.sub(this.vel)
+            steering.limit(this.maxForce)
+        }
+
+        return steering
+    }
+
+    flock(others) {
+        let alignment = this.align(others)
+        let cohesion = this.cohere(others)
+        let separation = this.separate(others)
+        this.accel.add(alignment)
+        this.accel.add(cohesion)
+        this.accel.add(separation)
     }
 
     move() {
-        
+        // update position, velocity, and acceleration
+        this.accel.set()
+        this.flock(boids)
+
+        this.vel.add(this.accel)
+        this.pos.add(this.vel)
+
+        // loop around the edges
+        if (this.pos.x > width) this.pos.x = 0
+        if (this.pos.x < 0) this.pos.x = width
+        if (this.pos.y > height) this.pos.y = 0
+        if (this.pos.y < 0) this.pos.y = height
     }
 
     show() {
+        noStroke()
         fill(112, 174, 110)
-        circle(this.x, this.y, 5)
+        circle(this.pos.x, this.pos.y, size)
     }
 }
 
+
 function setup() {
     let canvas = createCanvas(width, height)
+    console.log(`Successfully created canvas with dims {width, height}: {${width}, ${height}}`)
     canvas.parent('boids')
     angleMode(DEGREES)
 
     background(34, 29, 35)
 
     fill(112, 174, 110)
-    for (let i = 0; i < 100; i++) {
+
+    for (let i = 0; i < boid_count; i++) {
         boids.push(new Boid(random(width), random(height)))
     }
 }
@@ -43,6 +147,7 @@ function setup() {
 function draw() {
     background(34, 29, 35)
 
+    // enumerate over boid and the others
     for (let boid of boids) {
         boid.move()
         boid.show()
--- simulation/boids.md
+++ simulation/boids.md
@@ -1,13 +1,99 @@
 # Boids
-Boids is an artificial life program simulating flocking that was created by Craig Reynolds in 1986. Each individual boid moves according to three "steering behaviors":
-
-1. **Separation**: Avoid crowding local flockmates
-2. **Alignment**: Align with the average heading of local flockmates
-3. **Cohesion**: Move toward the average position of local flockmates
-
-Each boid has access to the rest of the flock's position/velocity, but adjusts according to flockmates in a neighborhood around it. The neighborhood is defined by a distance and angle from the boid's position and heading.
-
 ::boids
+
+## About
+Boids is an artificial life program simulating flocking that was created by Craig Reynolds in 1986. Each boid has access to the rest of the flock's position/velocity, but adjusts according to flockmates in a neighborhood around it. The neighborhood is defined by a distance and angle from the boid's position and heading.
+
+The `Boid` class contains the following traits:
+- `pos`: position vector (can get heading from `p5.Vector.heading()`)
+- `vel`: velocity vector
+- `accel`: acceleration vector
+- `maxVel`: maximum velocity
+- `separation_distance`: distance from boid to consider flockmates
+- `angle`: angle of the boid's vision from their heading
+
+## Steering
+Each individual boid moves according to three "steering behaviors":
+
+1. **Alignment**: Align with the average heading of local flockmates
+```javascript
+function align(others) {
+    let steering = createVector()
+    let total = 0
+    for (let other of others) {
+        let d = dist(this.pos.x, this.pos.y, other.pos.x, other.pos.y)
+
+        // add to steering if in neighborhood
+        if (other != this && d < this.angle) {
+            steering.add(other.vel)
+            total++
+        }
+    }
+    
+    // align with the average heading if flockmates in neighborhood
+    if (total > 0) {
+        steering.div(total)
+        steering.setMag(this.maxVel)
+        steering.sub(this.vel)
+        steering.limit(this.maxForce)
+    }
+
+    return steering
+}
+```
+2. **Cohesion**: Move toward the average position of local flockmates
+```javascript
+function cohere(others) {
+    let steering = createVector()
+    let total = 0
+    for (let other of others) {
+        // get distance between this boid and the other
+        let d = dist(this.pos.x, this.pos.y, other.pos.x, other.pos.y)
+        if (other != this && d < this.angle) {
+            steering.add(other.pos)
+            total++
+        }
+    }
+
+    // move towards the average position of flockmates in neighborhood
+    if (total > 0) {
+        steering.div(total)
+        steering.sub(this.pos)
+        steering.setMag(this.maxVel)
+        steering.sub(this.vel)
+        steering.limit(this.maxForce)
+    }
+
+    return steering
+}
+```
+3. **Separation**: Avoid crowding local flockmates
+```javascript
+separate(others) {
+    let steering = createVector()
+    let total = 0
+    for (let other of others) {
+        let d = dist(this.pos.x, this.pos.y, other.pos.x, other.pos.y)
+        if (other != this && d < separation_distance) {
+            // track the difference between the two positions
+            let diff = p5.Vector.sub(this.pos, other.pos)
+            diff.div(d)
+            steering.add(diff)
+            total++
+        }
+    }
+
+    // move away from nearby flockmates
+    if (total > 0) {
+        steering.div(total)
+        steering.setMag(this.maxVel)
+        steering.sub(this.vel)
+        steering.limit(this.maxForce)
+    }
+
+    return steering
+}
+```
 
 ### Sources
 - [Craig Reynolds](http://www.red3d.com/cwr/boids/)
@@ -15,3 +101,7 @@ Each boid has access to the rest of the flock's position/velocity, but adjusts a
 - [Boids Algorithm](https://vanhunteradams.com/Pico/Animal_Movement/Boids-algorithm.html)
 - [p5.min.js](https://cdnjs.cloudflare.com/ajax/libs/p5.js/1.9.2/p5.min.js)
 - [boids.js](/simulation/boids.js)
+
+### Navigation
+- [Home](/README.md)
+- [Simulation](/simulation)
